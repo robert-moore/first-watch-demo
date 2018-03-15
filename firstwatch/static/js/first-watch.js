e99bb557baff891471caea068f621dcf149a8e15
Rob Moore <rcmoore38@gmail.com>
2018-03-15 16:35:07 -0700

    select text

--- firstwatch/static/js/first-watch.js
+++ firstwatch/static/js/first-watch.js
@@ -1,24 +1,22 @@
 var dateParse = d3.timeParse("%Y-%m-%d %H:%M:%S")
 
-d3.select("#in-county-hospitals")
-    .selectAll("option")
-    .data(inCountyHospitals)
-    .enter()
-    .append("option")
-    .attr("value", function(d) { return d })
-    .attr("text", function(d) { return d })
 var screenWidth = window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
 
 var defaultWidth = 2000
 
 // needed because of background hospital image
 var scaleFactor = screenWidth / defaultWidth
 
-d3.select("#replay-vis")
-    .style("zoom", scaleFactor)
-    .style("-moz-transform", "scale(" + scaleFactor + ")")
+
+d3.select("#in-county-hospitals")
+    .selectAll("option")
+    .data(inCountyHospitals)
+    .enter()
+    .append("option")
+    .attr("value", function(d) { return d })
+    .text( function(d) { return d })
 
 d3.select("#out-of-county-hospitals")
     .selectAll("option")
@@ -26,17 +24,16 @@ d3.select("#out-of-county-hospitals")
     .enter()
     .append("option")
     .attr("value", function(d) { return d })
-    .attr("text", function(d) { return d })
+    .text( function(d) { return d })
+
+d3.select("#replay-vis")
+    .style("zoom", scaleFactor)
+    .style("-moz-transform", "scale(" + scaleFactor + ")")
+
 
 d3.select("#facility-selector")
     .on('change', setDateAndFacility)
 
-function setDateAndFacility() {
-    // var selectedDate = d3.select('#date-selector').property('value'); 
-    var selectedDate = "02-12-2018"
-    var selectedFacility = d3.select('#facility-selector').property('value');
-    window.location.href = '/hospital-data/' + selectedFacility + '/' + selectedDate 
-}
 
 var scatterVisData = []
 for (var i = 0; i < scatterData.times.length; i++) {
@@ -48,3 +45,10 @@ for (var i = 0; i < scatterData.times.length; i++) {
 drawScatterVis(scatterVisData, d3.select("#scatter-vis"))
 drawChangesVis(changesData, d3.select("#changes-vis"))
 drawFractionsVis(fractionsData, d3.select("#fractions-vis"))
+
+function setDateAndFacility() {
+    // var selectedDate = d3.select('#date-selector').property('value'); 
+    var selectedDate = "02-12-2018"
+    var selectedFacility = d3.select('#facility-selector').property('value');
+    window.location.href = '/hospital-data/' + selectedFacility + '/' + selectedDate 
+}
